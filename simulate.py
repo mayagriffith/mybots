629a8f09d9b63bc4826586f8c3c7f556268fae22
mayagriffith <mayaegriffith@gmail.com>
2023-01-18 13:02:33 -0500

    simulation running

--- simulate.py
+++ simulate.py
@@ -1,5 +1,12 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for iter in range(0,1000):
+    p.stepSimulation()
+    print(iter)
+    time.sleep(1/60)
+
 p.disconnect()
 
