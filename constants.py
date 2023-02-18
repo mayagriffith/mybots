28109a55bc9dd276bee04767f55efdd1f68fdd6d
Maya Griffith <mayagriffith@MacBook-Pro-3302.local>
2023-02-18 16:26:03 -0500

    git problems lol

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 import math
 gravity = -90.8
@@ -10,6 +9,3 @@ phaseOffsetFront = 0
 amplitudeBack = pi/4
 frequencyBack = 8
 phaseOffsetBack = pi/4
-=======
-gravity = -90.8
->>>>>>> 9751369 (deleting anyways)
