e289b0c486767c53152437fef528ff3ad1400c35
Maya Griffith <mayagriffith@ip0af5c16d.int.uvm.edu>
2023-02-23 12:16:37 -0500

    did first section

--- generate.py
+++ generate.py
@@ -36,15 +36,8 @@ def Create_7link_Robot():
 
     pyrosim.End()
 
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
-
-    # pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5] , size=[1,1,1])
-    # pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = [0.5,0,1])
-    # pyrosim.Send_Cube(name="Torso", pos=[0.5,0,0.5] , size=[1,1,1])
-    # pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,0])
-    # pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1,1,1])
-
     pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [.5,0,1])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-.5] , size=[1,1,1])
@@ -56,9 +49,20 @@ def Create_Robot():
 
     pyrosim.End()
 
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+
+    pyrosim.End()
+
+  
+
+
+
 
 Create_World()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
 # Create_7link_Robot()
 
 
