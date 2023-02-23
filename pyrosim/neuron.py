e50a995d23558fa1c46a5ea976951bd74b69f4d6
Maya Griffith <mayagriffith@ip0af5c16d.int.uvm.edu>
2023-02-23 12:42:58 -0500

    finish simulating sensor neurons

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -67,7 +67,8 @@ class NEURON:
         self.value = value
 
     def Update_Sensor_Neuron(self):
-        pass
+        self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link
+                       (self.Get_Link_Name()))
 
 # -------------------------- Private methods -------------------------
 
