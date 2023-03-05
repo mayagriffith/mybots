e5f55244ceef006c5f981da118d53fca8d386884
Maya Griffith <mayagriffith@MacBook-Pro-3302.local>
2023-03-05 17:11:30 -0500

    did generating a synaspse

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -70,8 +70,21 @@ class NEURON:
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link
                        (self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(math.pi/4)
+        print("currently updating " + self.Get_Name())
+        print("has value of " + str(self.Get_Value()))
+        for key in synapses:
+             if (key[1] == self.Get_Name()):
+                 self.Allow_Presynaptic_Neuron_To_Influence_Me(
+                    synapses[key].Get_Weight(),
+                    neurons[key[0]].Get_Value())
+        
+        print("now has value of " + str(self.Get_Value()))
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, weight, value):
+        self.Add_To_Value(value * weight)
 
 # -------------------------- Private methods -------------------------
 
