06df60dad3f5f06e2c8a1f2235567b2aaf320fba
Maya Griffith <mayagriffith@MacBook-Pro-3302.local>
2023-03-08 21:55:04 -0500

    spawn

--- hillclimber.py
+++ hillclimber.py
@@ -1,5 +1,6 @@
 from solution import SOLUTION
 import constants as c
+import copy
 
 class HILL_CLIMBER:
     def __init__(self):
@@ -15,7 +16,7 @@ class HILL_CLIMBER:
             self.Select()
 
     def Spawn(self):
-        pass
+        self.child = copy.deepcopy(self.parent)
 
     def Mutate(self):
         pass
