0dd981660d9bb8e928cab6cd91ecbfa65084216c
Maya Griffith <mayagriffith@MacBook-Pro-3302.local>
2023-03-08 21:24:57 -0500

    evaluate done.

--- hillclimber.py
+++ hillclimber.py
@@ -4,5 +4,5 @@ class HILL_CLIMBER:
     def __init__(self):
         self.parent = SOLUTION()
 
-    def Evolve():
-        pass
+    def Evolve(self):
+        self.parent.Evaluate()
--- solution.py
+++ solution.py
@@ -1,4 +1,12 @@
 import numpy as np
+import pyrosim.pyrosim as pyrosim
+import random
+import os
+
+
+length = 1
+width = 1
+height = 1
 
 class SOLUTION:
     def __init__(self):
@@ -13,4 +21,42 @@ class SOLUTION:
         self.weights = matrix
         self.weights = self.weights* 2 - 1
 
-sol = SOLUTION()
+    
+    def Evaluate(self):
+        self.Create_World()
+        self.Generate_Body()
+        self.Generate_Brain()
+        os.system("python3 simulate.py")
+
+    def Create_World(self):
+        pyrosim.Start_SDF("world.sdf")
+        pyrosim.Send_Cube(name="Box", pos=[-2,2,.5] , size=[length,width,height])
+        pyrosim.End()
+
+    def Generate_Body(self):
+        pyrosim.Start_URDF("body.urdf")
+        pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[1,1,1])
+        pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [.5,0,1])
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-.5] , size=[1,1,1])
+        pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-.5,0,1])
+        pyrosim.Send_Cube(name="BackLeg", pos=[-.5,0,-0.5] , size=[1,1,1])
+        pyrosim.End()
+
+    def Generate_Brain(self):
+        pyrosim.Start_NeuralNetwork("brain.nndf")
+        pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+        pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+        pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+        pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+
+
+        for currentRow in range(3):
+                for currentColumn in range(2):
+                    pyrosim.Send_Synapse(sourceNeuronName = currentRow ,
+                                        targetNeuronName = currentColumn + 3 ,
+                                        weight = self.weights[currentRow][currentColumn])
+        pyrosim.End()
+
+    
+
